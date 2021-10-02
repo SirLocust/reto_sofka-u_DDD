4e3179fdb97c86d6417eed4911d3c9efec78fab5
sirlocust <jua56yu@hotmail.com>
2021-10-01 19:03:21 -0500

    base del proyecto

---
+++ src/main/java/com/devsirlocust/challenger/ddd/DddApplication.java
@@ -0,0 +1,13 @@
+package com.devsirlocust.challenger.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddApplication.class, args);
+	}
+
+}
