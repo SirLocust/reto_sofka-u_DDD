c899e5311f45b9f497388b98f97053988c6c3fef
sirlocust <jua56yu@hotmail.com>
2021-10-01 22:21:56 -0500

    fix name event

--- src/main/java/com/devsirlocust/challenger/ddd/domain/poster/event/PosterCreated.java
+++ src/main/java/com/devsirlocust/challenger/ddd/domain/poster/event/PosterCreated.java
@@ -7,7 +7,7 @@ import com.devsirlocust.challenger.ddd.domain.poster.values.object.NickName;
 public class PosterCreated extends DomainEvent {
     private final NickName nickName;
     public PosterCreated(NickName nickName) {
-        super("devsirlocust.post.postcreated");
+        super("devsirlocust.poster.postercreated");
         this.nickName = nickName;
     }
 
